e1e3ca1b0fc0ad8b82392d4daee2fa302d1eae91
Ineida <48807622+Ineida@users.noreply.github.com>
2019-03-22 16:32:02 +0100

    premier commit

---
+++ src/tp/arithmétique/Cardoso_Viraragavane.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.arithmétique;
+
+/**
+ *
+ * @author cardo
+ */
+public class Cardoso_Viraragavane {
+    public int pgcd(int a, int b){
+        int b;
+        return b;
+    };
+    public int[][]Euclide(int a,int b){
+        int[][] resultat;
+        
+        return resultat;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
